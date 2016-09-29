800cb64ba993611f63d2263ed422e5b5254a62f8
WildMissingNo <underscorewildmissingno@gmail.com>
2016-09-29 15:23:30 +0930

    Delete SetListWrapper.cs

--- Scripts/core/SetListWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-
-[System.Serializable]
-public class SetListWrapper : MonoBehaviour 
-{
-	public List<string> s;
-}
